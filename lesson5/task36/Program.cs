272f1f6c07670f9dced2a4da126915cd559f48b3
unknown <post-design@mail.ru>
2022-07-20 23:40:08 +0800

    добавила задания к 5 уроку

---
+++ lesson5/task36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,100);
+}
+ 
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    sum += array[i];
+}
+
+Console.WriteLine(string.Join (",", array));
+
+Console.WriteLine(sum);
---
+++ lesson5/task38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,99);
+}
+
+double max = double.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    max = array[i];
+}
+
+double min = double.MaxValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    min = array[i];
+}
+
+double result = max - min;
+
+Console.WriteLine(string.Join (",", array));
+
+Console.WriteLine($"Максимальное число {max} - минимальное {min} = разница {result}");
